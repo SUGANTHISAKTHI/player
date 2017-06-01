92df8aa3340cebec96fb10004e13aa6e54b262c0
SUGANTHISAKTHI <suganthi.scse@ksrce.ac.in>
2017-06-01 08:32:24 -0700

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+package string;
+import java.util.Scanner;
+public class string {
+public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	String s1=s.nextLine();
+	int n=Integer.parseInt(s1);
+	s.close();
+	System.out.println("the intger is"+n);
+}
+}
